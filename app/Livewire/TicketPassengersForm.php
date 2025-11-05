5e150543a447f83b0cdfeeec605b37b714d066ea
YSRoot <ualeroev@bk.ru>
2025-11-05 20:17:11 -0300

    rework ticket passengers forms

--- app/Livewire/TicketPassengersForm.php
+++ app/Livewire/TicketPassengersForm.php
@@ -6,9 +6,15 @@ use Filament\Actions\Action;
 use Filament\Actions\Concerns\CanOpenModal;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\EmptyState;
 use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\Size;
 use Filament\Support\Icons\Heroicon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -43,57 +49,117 @@ final class TicketPassengersForm extends Component implements HasForms, HasActio
     {
         return $schema
             ->components([
-                Forms\Components\Repeater::make('passengers')
-                    ->label('Пассажиры')
-                    ->itemNumbers()
-                    ->itemLabel('Пассажир')
+                Grid::make(4)
                     ->schema([
-                        Section::make()
-                            ->icon(Heroicon::OutlinedUser)
-                            ->description('Введите данные пассажира для оформления билета')
+                        Group::make()
                             ->schema([
-                                Grid::make()->schema([
-                                    Forms\Components\TextInput::make('first_name')
-                                        ->label('Имя')
-                                        ->default(fake()->firstName())
-                                        ->required(),
-                                    Forms\Components\TextInput::make('last_name')
-                                        ->label('Фамилия')
-                                        ->default(fake()->lastName())
-                                        ->required(),
-                                    Forms\Components\DatePicker::make('birthdate')
-                                        ->label('Дата рождения')
-                                        ->default(fake()->date())
-                                        ->required(),
-                                    Forms\Components\TextInput::make('document')
-                                        ->label('Паспорт')
-                                        ->default(fake()->numerify('#######'))
-                                        ->required(),
-                                ]),
+                                Section::make('Информация о рейсе')
+                                    ->icon(Heroicon::Ticket)
+                                    ->description('Здесь будет информация о рейсе')
+                                    ->schema([
+                                        EmptyState::make('Данные рейса: ')
+                                            ->description('Здесь будет информация о рейсе.')
+                                            ->icon('gmdi-airplanemode-active')
+                                    ]),
+                                Section::make('Введите данные пассажира для оформления билета')
+                                    ->icon(Heroicon::OutlinedUser)
+                                    ->schema([
+                                        Forms\Components\Repeater::make('passengers')
+                                            ->label('Пассажиры')
+                                            ->itemNumbers()
+                                            ->itemLabel('Пассажир')
+                                            ->schema([
+                                                Grid::make()->schema([
+                                                    Forms\Components\TextInput::make('first_name')
+                                                        ->label('Имя')
+                                                        ->default(fake()->firstName())
+                                                        ->required(),
+                                                    Forms\Components\TextInput::make('last_name')
+                                                        ->label('Фамилия')
+                                                        ->default(fake()->lastName())
+                                                        ->required(),
+                                                    Forms\Components\DatePicker::make('birthdate')
+                                                        ->label('Дата рождения')
+                                                        ->default(fake()->date())
+                                                        ->required(),
+                                                    Forms\Components\TextInput::make('document')
+                                                        ->label('Паспорт')
+                                                        ->default(fake()->numerify('#######'))
+                                                        ->required(),
+                                                ]),
+                                            ])
+                                            ->defaultItems(fn () => (int) session('ticket_form.passenger_count', 1))
+                                            ->minItems(1)
+                                            ->columns(1)
+                                            ->reorderable(false)
+                                            ->addAction(fn(Action $action) => $action
+                                                ->label('Добавить пассажира')
+                                                ->icon(Heroicon::UserPlus)
+                                            )
+                                            ->cloneable()
+                                            ->deleteAction(fn(Action $action) => $action
+                                                ->label('Удалить пассажира')
+                                                ->icon(Heroicon::XMark)
+                                                ->requiresConfirmation()
+                                                ->modalHeading('Удалить пассажира?')
+                                                ->modalDescription('Вы уверены, что хотите удалить этого пассажира?')
+                                                ->modalSubmitActionLabel('Удалить')
+                                                ->modalCancelActionLabel('Отмена')
+                                            )
+                                            ->columnSpanFull(),
+                                    ]),
+                                Section::make('Контактная информация')
+                                    ->aside()
+                                    ->icon(Heroicon::EnvelopeOpen)
+                                    ->description('На e-mail вышлем маршрут-квитанцию электронного билета после оплаты')
+                                    ->schema([
+                                        TextInput::make('email')
+                                            ->label('E-mail')
+                                            ->required()
+                                            ->email(),
+                                    ]),
                             ])
-                            ->collapsible(false)
-                            ->compact()
-                        ,
-                    ])
-                    ->defaultItems(fn () => (int) session('ticket_form.passenger_count', 1))
-                    ->minItems(1)
-                    ->columns(1)
-                    ->reorderable(false)
-                    ->addAction(fn(Action $action) => $action
-                        ->label('Добавить пассажира')
-                        ->icon(Heroicon::UserPlus)
-                    )
-                    ->cloneable()
-                    ->deleteAction(fn(Action $action) => $action
-                        ->label('Удалить пассажира')
-                        ->icon(Heroicon::UserMinus)
-                        ->requiresConfirmation()
-                        ->modalHeading('Удалить пассажира?')
-                        ->modalDescription('Вы уверены, что хотите удалить этого пассажира?')
-                        ->modalSubmitActionLabel('Удалить')
-                        ->modalCancelActionLabel('Отмена')
-                    )
-                    ->columnSpanFull(),
+                            ->columnSpan(3),
+                        Section::make()
+                            ->schema([
+                                EmptyState::make('Стоимость')
+                                    ->description('Здесь будет итоговая сводка по стоимости.')
+                                    ->icon(Heroicon::CurrencyDollar),
+                                Forms\Components\Select::make('currency')
+                                    ->label('Валюта для квитанции')
+                                    ->options([
+                                        'rub' => 'Российский рубль - ₽',
+                                    ])
+                                    ->default('rub')
+                                    ->required(),
+                                Radio::make('payment_method')
+                                    ->options([
+                                        'bank_ru'  => 'Банковская карта',
+                                        'bank_int' => 'Банковская карта',
+                                        'crypt'    => 'Криптовалюта',
+                                    ])
+                                    ->descriptions([
+                                        'bank_ru'  => 'Российский банк',
+                                        'bank_int' => 'Международный банк',
+                                        'crypt'    => 'USDT, BTC, Binance, Bybit, ...',
+                                    ]),
+                                Action::make('pay')
+                                    ->label('Перейти к оплате')
+                                    ->color('primary')
+                                    ->icon(Heroicon::DocumentCurrencyPound)
+                                    ->size(Size::ExtraLarge)
+                                    ->button()
+                                    ->extraAttributes(['class' => 'w-full'])
+                                    ->slideOver()
+                                    ->modalSubmitActionLabel('Оплатить')
+                                    ->schema([
+                                        EmptyState::make('Форма оплаты')
+                                            ->description('Здесь будет форма оплаты выбранным платежным методом.')
+                                            ->icon(Heroicon::DocumentCurrencyEuro),
+                                    ]),
+                            ])
+                            ->columnSpan(1),
+                    ]),
             ])
             ->statePath('data');
     }
--- resources/views/livewire/ticket-passengers-form.blade.php
+++ resources/views/livewire/ticket-passengers-form.blade.php
@@ -1,21 +1,9 @@
-<div class="max-w-4xl mx-auto px-4 py-12">
+<div class="max-w-7xl mx-auto px-4 py-12">
     <h1 class="text-3xl font-bold mb-2 text-center">Данные пассажиров ✈️</h1>
     <p class="text-gray-500 text-center mb-10">Заполните информацию для каждого пассажира</p>
 
     <x-filament-actions::modals />
 
-    <form wire:submit.prevent="submit" class="space-y-8">
-        {{ $this->form }}
+    {{ $this->form }}
 
-        <x-filament::button
-            type="submit"
-            class="w-full py-4 rounded-xl font-semibold text-lg text-white
-                       bg-gradient-to-r from-indigo-600 to-purple-600
-                       hover:from-indigo-700 hover:to-purple-700 transition shadow-lg
-                       flex justify-center"
-            wire:loading.attr="disabled"
-        >
-            Продолжить →
-        </x-filament::button>
-    </form>
 </div>
